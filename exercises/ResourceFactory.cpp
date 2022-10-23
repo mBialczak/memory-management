5a6232b0b8b994af67ac890c57f6f992adf297cc
Maciej Białczak <m.bialczak@protonmail.com>
2022-10-23 22:58:29 +0200

    First most ugly version with no memory problems

--- exercises/ResourceFactory.cpp
+++ exercises/ResourceFactory.cpp
@@ -1,67 +1,71 @@
 #include <iostream>
-#include <vector>
+#include <memory>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-struct Resource
-{
-    Resource(char* byte) : byte_(byte) {}
-    char* byte() const { return byte_; }
+struct Resource {
+    Resource(unique_ptr<char> byte)
+        : byte_(move(byte)) {}
+    char byte() const { return *byte_; }
     virtual string name() const = 0;
-    ~Resource() { delete byte_; }
 
 protected:
-    char* byte_ = nullptr;
+    unique_ptr<char> byte_;
 };
 
-struct ResourceA : Resource
-{
-    ResourceA(char* byte) : Resource(byte) {}
-    string name() const override { return string("ResourceA ").append(byte_); }
+struct ResourceA : Resource {
+    ResourceA(unique_ptr<char> byte)
+        : Resource(move(byte)) {}
+    // tries to append *char to string??
+    string name() const override {
+        return string("ResourceA ").append(byte_.get(), byte_.get() + 1);
+    }
 };
 
-struct ResourceB : Resource
-{
-    ResourceB(char* byte) : Resource(byte) {}
-    string name() const override { return string("ResourceB ").append(byte_); }
+struct ResourceB : Resource {
+    ResourceB(unique_ptr<char> byte)
+        : Resource(move(byte)) {}
+    string name() const override {
+        return string("ResourceB ").append(byte_.get(), byte_.get() + 1);
+    }
 };
 
-struct ResourceFactory
-{ 
-    Resource* makeResourceA(char* byte) { return new ResourceA{byte}; }
-    Resource* makeResourceB(char* byte) { return new ResourceB{byte}; }
+struct ResourceFactory {
+    std::shared_ptr<Resource>
+    makeResourceA(unique_ptr<char> byte) {
+        return std::make_shared<ResourceA>(move(byte));
+    }
+    std::shared_ptr<Resource> makeResourceB(unique_ptr<char> byte) {
+        return std::make_shared<ResourceB>(move(byte));
+    }
 };
 
-struct ResourceCollection
-{
-    void add(Resource* r) { resources.push_back(r); }
-    void clear() { resources.clear(); }
-    Resource* operator[](int index) { return resources[index]; }
-    void printAll()
-    {
-        for (const auto & res : resources)
-        {
+struct ResourceCollection {
+    void add(std::shared_ptr<Resource> r) { resources.push_back(r); }
+    void clear() { resources.clear(); }  // clearing what about deleting?
+    std::shared_ptr<Resource> operator[](int index) { return resources[index]; }
+    void printAll() {
+        for (const auto& res : resources) {
             cout << res->name() << endl;
         }
     }
 
 private:
-    vector<Resource*> resources;
+    vector<std::shared_ptr<Resource>> resources;
 };
 
-int main()
-{
+int main() {
     ResourceCollection collection;
     ResourceFactory rf;
-    collection.add(rf.makeResourceA(new char{0x01}));
-    collection.add(rf.makeResourceB(new char{0x02}));
+    collection.add(rf.makeResourceA(make_unique<char>(0x01)));
+    collection.add(rf.makeResourceB(make_unique<char>(0x02)));
     collection.printAll();
 
     auto firstByte = collection[0]->byte();
     collection.clear();
-    cout << *firstByte << endl;
+    cout << firstByte << endl;
 
     return 0;
 }
-
