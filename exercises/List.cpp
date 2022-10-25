1b3f0d8a8200c0419442c9a4d47a9c8e1c7e8b5b
Maciej Białczak <m.bialczak@protonmail.com>
2022-10-25 20:46:19 +0200

    Completed fix using raw pointer

--- exercises/List.cpp
+++ exercises/List.cpp
@@ -2,22 +2,20 @@
 
 using namespace std;
 
-class Node
-{
+class Node {
 public:
-    Node(const int v) :
-        next(nullptr),
-        value(v)
-    {}
+    Node(const int v)
+        : next(nullptr),
+          value(v) {}
 
     Node* next;
     int value;
 };
 
-class List
-{
+class List {
 public:
     List();
+    ~List();
     void add(Node* node);
     Node* get(const int value);
 
@@ -25,57 +23,50 @@ private:
     Node* first;
 };
 
-List::List() :
-    first(nullptr)
-{}
+List::List()
+    : first(nullptr) {}
 
-void List::add(Node* node)
-{
-    if(!first)
-    {
-        first = node;
+List::~List() {
+    while (first) {
+        Node* new_first = first->next;
+        delete first;
+        first = new_first;
     }
-    else
-    {
+}
+
+void List::add(Node* node) {
+    if (!first) {
+        first = node;
+    } else {
         Node* current = first;
-        while(current->next)
-        {
+        while (current->next) {
             current = current->next;
         }
         current->next = node;
     }
 }
 
-Node* List::get(const int value)
-{
-    if(!first)
-    {
+Node* List::get(const int value) {
+    if (!first) {
         cout << "List is empty!" << endl;
         return nullptr;
-    }
-    else
-    {
+    } else {
         Node* current = first;
-        do
-        {
-            if(current->value == value)
-            {
+        do {
+            if (current->value == value) {
                 cout << "Found value " << current->value << endl;
                 return current;
-            }
-            else
-            {
+            } else {
                 cout << "Going through " << current->value << endl;
                 current = current->next;
             }
-        } while(current);
+        } while (current);
         cout << "Not found: value " << value << endl;
         return nullptr;
     }
 }
 
-int main()
-{
+int main() {
     List lista;
     Node* node4 = new Node(4);
     Node* node7 = new Node(7);
@@ -91,4 +82,3 @@ int main()
 
     return 0;
 }
-
